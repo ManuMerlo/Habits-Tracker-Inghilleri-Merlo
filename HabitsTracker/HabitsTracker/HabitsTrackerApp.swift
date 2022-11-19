65aec2f66e84ee0994c99a048595d49b11629f24
Riccardo Inghilleri <riccardo.inghilleri@gmail.com>
2022-11-19 17:43:33 +0100

    Created Xcode Project

---
+++ HabitsTracker/HabitsTracker/HabitsTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitsTrackerApp.swift
+//  HabitsTracker
+//
+//  Created by Riccardo Inghilleri on 19/11/22.
+//
+
+import SwiftUI
+
+@main
+struct HabitsTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
