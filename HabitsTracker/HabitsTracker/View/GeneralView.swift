72b6c2c942ff7491105602edeb8aa0815bce2256
Manuela Merlo <merlomanu1999@gmail.com>
2023-04-02 09:32:40 +0200

    Update Leaderboard view

--- HabitsTracker/HabitsTracker/View/GeneralView.swift
+++ HabitsTracker/HabitsTracker/View/GeneralView.swift
@@ -13,26 +13,31 @@ import GoogleSignIn
 struct GeneralView: View {
     var body: some View {
         TabView {
+            
             HomeView()
                 .tabItem {
                     Image(systemName: "house")
                     Text("Home")
                 }
+            
             PlanningView()
                 .tabItem {
                     Image(systemName: "calendar")
                     Text("Planning")
                 }
+            
             LeaderboardView()
                 .tabItem {
                     Image(systemName: "trophy")
                     Text("Leaderboard")
                 }
+            
             Text("Goals")
                 .tabItem {
                     Image(systemName: "medal")
                     Text("Goals")
                 }
+            
             SettingsView()
                 .tabItem {
                     Image(systemName: "gear")
--- HabitsTracker/HabitsTracker/View/LeaderboardView.swift
+++ HabitsTracker/HabitsTracker/View/LeaderboardView.swift
@@ -12,19 +12,25 @@ struct LeaderboardView: View {
     
     @State private var selectedTimeFrame : TimeFrame = .daily
     var body: some View {
+        
         GeometryReader { geometry in
+
             VStack {
+                
                 Text("Leaderboard")
                     .font(.largeTitle)
                     .fontWeight(.bold)
                     .frame(maxWidth: .infinity, alignment: .leading)
                     .padding(.horizontal, 20)
+                
+                
                 Picker("Choose a time frame",selection: $selectedTimeFrame) {
                     ForEach(TimeFrame.allCases,id: \.self){
                         Text($0.rawValue)
                     }
                 }.pickerStyle(SegmentedPickerStyle())
-                    .padding([.leading, .bottom, .trailing])
+                .padding(.bottom,30)
+                .padding(.horizontal,20)
                
                 //TODO : da implementare
                 /*switch selectedTimeFrame {
@@ -34,50 +40,76 @@ struct LeaderboardView: View {
                     users = viewModel.dailyUsers
                 }*/
                 
-                HStack (spacing: 7) {
-                    RankingCard(position: "2nd",
+                HStack{
+                    RankingCard(position: "2",
                                 image_path: "Avatar 2",
                                 username: "Player 2",
                                 score: 400,
-                                width: (geometry.size.width - 60) * 0.328,
-                                height: geometry.size.height/4,
-                                color: CommodityColor.silver.linearGradient)
-                    RankingCard(position: "1st",
-                                image_path: "Avatar 1",
-                                username: "Player 1",
-                                score: 500,
-                                width: (geometry.size.width - 60) * 0.364,
-                                height: geometry.size.height/3.5,
-                                color: CommodityColor.gold.linearGradient)
-                    RankingCard(position: "3rd",
+                                width: (geometry.size.width - 70) * 0.4,
+                                color: CommodityColor.silver.linearGradient,
+                                squareSide: geometry.size.width - 290)
+                    .position(x:geometry.size.width/4,
+                              y:geometry.size.height/4.8)
+                    
+                    RankingCard(position: "3",
                                 image_path: "Avatar 3",
                                 username: "Player 3",
                                 score: 300,
-                                width: (geometry.size.width - 60) * 0.308,
-                                height: geometry.size.height/4.5,
-                                color: CommodityColor.bronze.linearGradient)
+                                width: (geometry.size.width - 70) * 0.4,
+                                color: CommodityColor.bronze.linearGradient,
+                                squareSide:geometry.size.width - 290)
+                    .position(x:geometry.size.width/2.4,
+                              y:geometry.size.height/4.8)
+                    
+                    RankingCard(position: "1",
+                                image_path: "Avatar 4",
+                                username: "Player 1",
+                                score: 500,
+                                width: (geometry.size.width - 70) * 0.5,
+                                color: CommodityColor.gold.linearGradient,
+                                squareSide:geometry.size.width - 260)
+                    .position(x:-geometry.size.width/5.6,
+                              y:geometry.size.height/8)
+                    
                 }
-                List(0..<20) { item in
-                    HStack {
-                        Text("\(item + 4)").frame(width: geometry.size.width / 15)
-                            .font(.title3)
-                            .fontWeight(.bold)
-                        Spacer()
-                        Image("Avatar 1")
-                            .resizable()
-                            .frame(width: 40, height: 40)
-                            .mask(Circle())
-                            .padding(.trailing, 8.0)
-                        
-                        Text("username").padding(.trailing)
-                        Spacer()
-                        Text("300 pt")
-                            .fontWeight(.bold)
+               
+                ScrollView {
+                    LazyVStack {
+                        ForEach(0..<20) { item in
+                            HStack(spacing: 20) {
+                                
+                                VStack {
+                                    Text("\(item + 4)")
+                                        .font(.callout)
+                                        .fontWeight(.bold)
+                                        .frame(width: geometry.size.width / 15)
+                                    Text("⏷")
+                                }.padding(.horizontal,10)
+                                
+                                Image("Avatar 1")
+                                    .resizable()
+                                    .frame(width: 50, height: 50)
+                                    .mask(Circle())
+                                Text("username")
+                                    .font(.body)
+                                    .fontWeight(.bold)
+                                Divider()
+                                Text("300 ")
+                                    .fontWeight(.bold)
+                            
+                            }
+                            .padding(.vertical,10)
+                            .frame(maxWidth: .infinity, maxHeight: 80)
+                            .background(item % 2 == 0 ? Color.green : .purple)
+                            .foregroundColor(.white)
+                            .mask(RoundedRectangle(cornerRadius: 20, style: .continuous))
+                        }.padding(.horizontal,20)
                     }
-                }.scrollContentBackground(.hidden)
-            }
+                }
             
+            }
         }
+        
     }
 }
 
@@ -98,33 +130,63 @@ struct RankingCard: View {
     let username: String
     let score: Int
     let width: CGFloat
-    let height: CGFloat
     let color: LinearGradient
+    let squareSide: CGFloat
     
     var body: some View {
-        VStack() {
-            Spacer()
-            Text(position)
-            Spacer()
-            Image(image_path)
-                .resizable()
-                .frame(width: width * 0.65, height:  width * 0.65)
-                .mask(Circle())
-            Spacer()
+        
+        VStack {
+           
+            if position == "1"{
+                Image("crown")
+                    .resizable()
+                    .frame(width: 50,height: 50)
+                    .scaledToFit()
+            } else {
+                Text(position)
+                    .foregroundColor(.black)
+                    .font(.title3)
+                .bold()
+            }
+                
+            ImageOnCircle(icon: image_path, radius: width/2.2, circleColor: color, imageColor: .white,squareSide: squareSide)
             Text(username)
-            Spacer()
-            Text("\(score) pt")
-            Spacer()
-        }.foregroundColor(.black)
-            .padding(5)
-            .frame(width: width, height: height)
-            .background(color)
-            .mask(RoundedRectangle(cornerRadius: 30,style: .continuous))
-            .shadow(color: Color.gray.opacity(0.3), radius: 8, x: 0, y: 12)
-            .shadow(color: Color.gray.opacity(0.3), radius: 2, x: 0, y: 1)
+                .foregroundColor(.black)
+                .font(.title3)
+                .bold()
+            Text("\(score)")
+                .foregroundColor(.black)
+                .font(.callout)
+                .bold()
+        }
+       
     }
     
+}
+
+struct ImageOnCircle: View {
     
+    let icon: String
+    let radius: CGFloat
+    let circleColor: LinearGradient
+    let imageColor: Color
+    var squareSide: CGFloat
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(circleColor)
+                .frame(width: radius * 2, height: radius * 2)
+
+            Image(icon)
+               .resizable()
+               .mask(Circle())
+               .shadow(color: Color.gray.opacity(0.3), radius: 8, x: 0, y: 12)
+               .shadow(color: Color.gray.opacity(0.3), radius: 2, x: 0, y: 1)
+               .aspectRatio(1.0, contentMode: .fit)
+               .frame(width: squareSide, height: squareSide)
+        }
+    }
 }
 
 enum CommodityColor {
