806658a69b815fdddf1cda44bddebd95f63bc41e
Manuela Merlo <merlomanu1999@gmail.com>
2023-08-17 21:43:15 +0200

    Added portrait and iPad for IntroView

--- HabitsTracker/HabitsTracker/View/IntroView.swift
+++ HabitsTracker/HabitsTracker/View/IntroView.swift
@@ -6,67 +6,111 @@ struct IntroView: View {
     @ObservedObject var authenticationViewModel: AuthenticationViewModel
     @ObservedObject var firestoreViewModel = FirestoreViewModel()
     
-    
     let timer = Timer.publish(every: 3, on: .main, in: .common).autoconnect()
     
+    @Environment (\.verticalSizeClass) var verticalSizeClass
+   
+    @State private var isLandscape: Bool = false
+    @State private var device : Device = UIDevice.current.userInterfaceIdiom == .pad ? .iPad : .iPhone
+    @State var height = UIScreen.main.bounds.height
+    @State var width = UIScreen.main.bounds.width
+
+
+    // Constants and Configurations
+    enum PageColor {
+        static let blue = Color.blue
+        static let purple = Color.purple
+        static let red = Color.red
+    }
+    
+    enum PageFilename {
+        static let notes = "notes"
+        static let share = "share"
+        static let planActivities = "plan_activities"
+
+    }
+    
     var body: some View {
-        
         NavigationStack {
-            ZStack{
-                RadialGradient(gradient: Gradient(colors: [Color("delftBlue"), Color("oxfordBlue")]), center: .center, startRadius: 5, endRadius: 500)
-                    .edgesIgnoringSafeArea(.all)
-                VStack{
-                    ZStack{
-                        TabView(selection: $selectedPage)
-                        {
-                            ForEach(0..<3) {
-                                index in DescriptionCard(card: data[index]).tag(index).offset(y:25)
-                            }
-                            
+            RadialGradient(gradient: Gradient(colors: [Color("delftBlue"), Color("oxfordBlue")]), center: .center, startRadius: 5, endRadius: 500)
+                .edgesIgnoringSafeArea(.all)
+                .overlay {
+                    if (!isLandscape){
+                        VStack {
+                            Spacer()
+                            UpperImage(background: getColorForSelectedPage(selectedPage))
+                            Spacer()
+                            TabViewBuilder()
+                            Spacer()
                         }
-                        .onAppear() {
-                            UIPageControl.appearance().currentPageIndicatorTintColor = .black
-                            UIPageControl.appearance().pageIndicatorTintColor = .gray
-                        }
-                        .offset(y:-10)
-                        .tabViewStyle(PageTabViewStyle(indexDisplayMode:.automatic))
-                        .onReceive(timer){ time in
-                            selectedPage = (selectedPage + 1) % 3
-                        }
-                        
-                        //Selected Pages
-                        if selectedPage == 0 {
-                            UpperImage(background: .blue, filename: "notes")
-                        }
-                        
-                        if selectedPage == 1 {
-                            UpperImage(background: .purple, filename: "share")
-                        }
-                        
-                        if selectedPage == 2 {
-                            UpperImage(background: .pink, filename: "plan_activities")
-                        }
-                        
+                    }else{
+                        HStack {
+                            Spacer()
+                            UpperImage(background: getColorForSelectedPage(selectedPage))
+                            Spacer()
+                            TabViewBuilder()
+                            Spacer()
+                        }.frame(width: width)
                     }
-                    
-                    NavigationLink {
-                        SigninView(authenticationViewModel: authenticationViewModel,firestoreViewModel: firestoreViewModel)
-                    } label: {
-                        Text("Skip")
-                            .font(.system(size:22))
-                            .fontWeight(.semibold)
-                            .frame(width: 150, height: 50)
-                            .background(Color.blue)
-                            .cornerRadius(10)
-                    }
-                    .padding(.bottom)
-                }
-            }.foregroundColor(.white)
+                }.foregroundColor(.white)
+        }
+        .onAppear {
+            if (verticalSizeClass == .compact) || (width > height && device == .iPad){
+                isLandscape = true
+            } else {
+                isLandscape = false
+            }
+            
+        }
+        .onChange(of: verticalSizeClass) { newSize in
+            isLandscape = newSize == .compact
+            height =  UIScreen.main.bounds.height
+            width = UIScreen.main.bounds.width
         }
     }
     
     @ViewBuilder
-    func DescriptionCard(card : Card) -> some View {
+    func TabViewBuilder() -> some View {
+        VStack(alignment: .center) {
+            Spacer()
+
+            TabView(selection: $selectedPage)
+            {
+                DescriptionCard(card: data[selectedPage])
+                    .padding()
+                    .tag(selectedPage)
+                    .frame(height: !isLandscape ? height/2 : width/2)
+                    .padding(.horizontal, isLandscape ? 40 : 0)
+                    .padding(.vertical, isLandscape ? 40 : 0)
+            }
+            .onAppear() {
+                UIPageControl.appearance().currentPageIndicatorTintColor = .black
+                UIPageControl.appearance().pageIndicatorTintColor = .gray
+            }
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+            .onReceive(timer) { time in
+                selectedPage = (selectedPage + 1) % 3
+            }
+            
+            Spacer()
+            
+            NavigationLink {
+                SigninView(authenticationViewModel: authenticationViewModel, firestoreViewModel: firestoreViewModel)
+            } label: {
+                Text("Skip")
+                    .font(.system(size: 22))
+                    .fontWeight(.semibold)
+                    .frame(width: 150, height: 50)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
+    
+            Spacer()
+        }.frame(height: !isLandscape ? height/3 : width/3)
+    }
+    
+    @ViewBuilder
+    func DescriptionCard(card: Card) -> some View {
         VStack(alignment: .center, spacing: 17.0) {
             Text(card.title)
                 .font(.title)
@@ -78,35 +122,134 @@ struct IntroView: View {
                 .fontWeight(.light)
                 .multilineTextAlignment(.center)
                 .frame(width: 300)
-            
-        }.padding(.horizontal, 40)
-            .offset(y: 160)
+        }
     }
     
     @ViewBuilder
-    func UpperImage(background: Color , filename: String) -> some View {
-        ZStack {
-            Circle().frame(width: 500, height: 500)
-                .offset(x: 0, y: -190)
-                .foregroundColor(background)
+    func UpperImage(background : Color) -> some View {
+        let fraction = device == .iPad ? 1.5 : 1
+        VStack{
+            ZStack {
+                if device == .iPhone{
+                    iPhoneLayout(background: background)
+                }
+                else {
+                    iPadLayout(background: background)
+                }
+            }
+        }.frame(
+            width: !isLandscape ? width/fraction : height/fraction,
+            height: !isLandscape ? height/3 : width/3)
+    }
+    
+    @ViewBuilder
+    func iPhoneLayout(background: Color) -> some View {
+        Circle()
+            .frame(width:!isLandscape ? height + 50 : width + 50,
+                   height: !isLandscape ? height + 50 : width + 50 )
+            .foregroundColor(background)
+            .offset(y: !isLandscape ? -height/3.5 : 0)
+            .offset(x: isLandscape ? -width/3 : 0)
+        
+        Circle()
+            .frame(width: !isLandscape ? height : width,
+                   height: !isLandscape ? height : width)
+            .foregroundColor(.white)
+            .offset(y: !isLandscape ? -height/3.5 : 0)
+            .offset(x: isLandscape ? -width/3 : 0)
+        
+        if selectedPage == 0 {
             
-            Circle()
-                .frame(width: 600, height: 600)
-                .foregroundColor(.white)
-                .offset(x: 0, y: -290)
-            
-            LottieView(filename: filename)
-                .frame(width: 450, height: 450)
+            LottieView(filename: PageFilename.notes)
+                .frame(width: !isLandscape ? height/2.5 : width/2.5, height: !isLandscape ? height/2 : width/2.5)
                 .shadow(color: .orange, radius: 1, x: 0, y: 0)
-                .clipShape(Circle())
-                .offset(x: 0, y: -180)
+                .padding(.trailing, isLandscape ? 120 : 0)
+                .padding(.bottom, !isLandscape ? 20 : 0)
+        } else if selectedPage == 1 {
+            
+            LottieView(filename: PageFilename.share)
+                .frame(width: !isLandscape ? height/2.5 : width/2.5, height: !isLandscape ? height/2 : width/2.5)
+                .shadow(color: .orange, radius: 1, x: 0, y: 0)
+                .padding(.trailing, isLandscape ? 120 : 0)
+                .padding(.bottom, !isLandscape ? 20 : 0)
+            
+        } else {
+            
+            LottieView(filename: PageFilename.planActivities)
+                .frame(width: !isLandscape ? height/2.5 : width/2.5, height: !isLandscape ? height/2 : width/2.5)
+                .shadow(color: .orange, radius: 1, x: 0, y: 0)
+                .padding(.trailing, isLandscape ? 120 : 0)
+                .padding(.bottom, !isLandscape ? 20 : 0)
         }
     }
+    
+    @ViewBuilder
+    func iPadLayout(background: Color) -> some View {
+        Circle()
+            .frame(width:!isLandscape ? height + 50 : width + 50,
+                   height: !isLandscape ? height + 50 : width + 50 )
+            .foregroundColor(background)
+            .offset(y: !isLandscape ? -height/4 : 0)
+            .offset(x: isLandscape ? -width/4 : 0)
+        
+        Circle()
+            .frame(width: !isLandscape ? height : width,
+                   height: !isLandscape ? height : width)
+            .foregroundColor(.white)
+            .offset(y: !isLandscape ? -height/4 : 0)
+            .offset(x: isLandscape ? -width/4 : 0)
+        
+        if selectedPage == 0 {
+            LottieView(filename: PageFilename.notes)
+                .frame(width: !isLandscape ? height/2 : width/2.5, height: !isLandscape ? height/2 : width/2.5)
+                .shadow(color: .orange, radius: 1, x: 0, y: 0)
+                .padding(.bottom, !isLandscape ? 60 : 0)
+
+        } else if selectedPage == 1 {
+            LottieView(filename: PageFilename.share)
+                .frame(width: !isLandscape ? height/2 : width/2.5, height: !isLandscape ? height/2 : width/2.5)
+                .shadow(color: .orange, radius: 1, x: 0, y: 0)
+                .padding(.bottom, !isLandscape ? 60 : 0)
+            
+        } else {
+            LottieView(filename: PageFilename.planActivities)
+                .frame(width: !isLandscape ? height/2 : width/2.5, height: !isLandscape ? height/2 : width/2.5)
+                .shadow(color: .orange, radius: 1, x: 0, y: 0)
+                .padding(.bottom, !isLandscape ? 60 : 0)
+        }
+    }
+
+
+    
+    // Helper Functions
+    func getColorForSelectedPage(_ page: Int) -> Color {
+        switch page {
+        case 0: return PageColor.blue
+        case 1: return PageColor.purple
+        case 2: return PageColor.red
+        default: return PageColor.blue
+        }
+    }
+    
+    func getFilenameForSelectedPage(_ page: Int) -> String {
+        switch page {
+        case 0: return PageFilename.notes
+        case 1: return PageFilename.share
+        case 2: return PageFilename.planActivities
+        default: return ""
+        }
+    }
+    
 }
+
+enum Device {
+    case iPhone
+    case iPad
+}
+
 
 struct IntroView_Previews: PreviewProvider {
     static var previews: some View {
-        IntroView(healthViewModel: HealthViewModel(),authenticationViewModel: AuthenticationViewModel(), firestoreViewModel: FirestoreViewModel()
-        )
+        IntroView(healthViewModel: HealthViewModel(), authenticationViewModel: AuthenticationViewModel(), firestoreViewModel: FirestoreViewModel())
     }
 }
