02df49ea3d78296faff8f2e933bf5e413ec2f98e
Riccardo Inghilleri <riccardo.inghilleri@gmail.com>
2023-08-25 07:03:21 +0200

    Added unit testing setup.

---
+++ HabitsTracker/HabitsTracker_Tests/AuthenticationViewModel_Tests.swift
@@ -0,0 +1,23 @@
+import XCTest
+@testable import HabitsTracker
+
+final class AuthenticationViewModel_Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func test_AuthenticationViewModel_showAlert_shouldBeTrue() {
+        // Given
+        
+        // When
+
+        // Then
+        
+    }
+
+}
